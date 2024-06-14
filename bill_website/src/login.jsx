ba4b0c7096bef237a568165441e8fd5f6ad07b0e
Arun3622 <97716134+Arun3622@users.noreply.github.com>
2024-06-14 12:35:08 +0530

    loginpage

--- bill_website/src/login.jsx
+++ bill_website/src/login.jsx
@@ -1,44 +1,54 @@
 import React, { useState } from 'react';
 import './login.css';
+import { FaUser, FaLock } from "react-icons/fa";
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+function Login() {
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className='body1'>
+      <div className={`bodylogin ${isLogin ? '' : 'shift-left'}`}>
+        {isLogin ? (
+          <div className='line'>
+            <h1>Login</h1>
+            <div className='textbox'>
+              <label><input placeholder='Username' type="text" /></label><FaUser className='icon' />
+            </div>
+            <div className='textbox'>
+              <label><input placeholder='Password' type="password" /></label><FaLock className='icon' />
+            </div>
+            <div className='checkbox'>
+              <label>
+                <input type='checkbox' /> Remember me
+              </label>
+              <a href='#' className='forgot' style={{ marginLeft: 'auto' }}>Forgot Password</a>
+            </div>
+            <button className='buttonlogin' type='submit'>Login</button>
+            <div className='register'>
+              <p>Don't have an account? <a href='#' onClick={() => setIsLogin(false)}>Register</a></p>
+            </div>
+          </div>
+        ) : (
+          <div className='line'>
+            <h1>Register</h1>
+            <div className='textbox'>
+              <label><input placeholder='Username' type="text" /></label><FaUser className='icon' />
+            </div>
+            <div className='textbox'>
+              <label><input placeholder='Email' type="email" /></label><FaUser className='icon' />
+            </div>
+            <div className='textbox'>
+              <label><input placeholder='Password' type="password" /></label><FaLock className='icon' />
+            </div>
+            <button className='buttonlogin' type='submit'>Register</button>
+            <div className='register'>
+              <p>Already have an account? <a href='#' onClick={() => setIsLogin(true)}>Login</a></p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default Login;
